3467004e1031a575e024a7cdb7dc107ecad16d6a
RackGamesDev <yesterdiscord7@gmail.com>
2024-09-24 13:31:34 +0200

    a

---
+++ datos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    def __str__(self):
+        return
--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'modulo',#AGREGANDO MODULO PERSONALIZADO (ver crear.txt)
+    'datos'
 ]
 
 MIDDLEWARE = [
