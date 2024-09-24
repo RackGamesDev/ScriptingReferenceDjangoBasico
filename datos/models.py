213c59b3d99aaceef9fc25f3335ea97d7d3c8753
RackGamesDev <yesterdiscord7@gmail.com>
2024-09-24 14:34:37 +0200

    m

--- datos/models.py
+++ datos/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Usuario(models.Model):
+#CADA VEZ QUE SE ACTUALICE ESTE ARCHIVO: python manage.py makemigrations ; python manage.py migrate
+
+class Usuario(models.Model):#creando un modelo para un objeto de la base de datos
+    nombre = models.CharField(max_length=12, null=False, default="a")#sus propiedades y como funcionan (automaticamente traducidas a sql)
+    dinero = models.IntegerField(default=0)
+    activo = models.BooleanField(default=False)
+    descripcion = models.TextField(default="", null=True)
+    fecha = models.DateField(null=True)
+
     def __str__(self):
         return
