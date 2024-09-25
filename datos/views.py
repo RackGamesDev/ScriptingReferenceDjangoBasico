2967f9f273aeaf9146ea33d52689418bd5b51aaf
RackGamesDev <yesterdiscord7@gmail.com>
2024-09-25 13:27:22 +0200

    a

--- datos/views.py
+++ datos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Usuario #importando el modelo de datos para la base de datos
 
 # Create your views here.
 
@@ -7,4 +8,6 @@ def test(request):
     return HttpResponse("OK")
 
 def create(request):
+    usuario = Usuario(nombre="prueba", dinero=0, activo=True, descripcion="") #creando un objeto segun el modelo
+    usuario.save() #guardandolo en la base de datos
     return HttpResponse("OK2")
