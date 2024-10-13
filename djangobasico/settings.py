1a84c016e65fefccf03d1cf1184256888f6402ae
RackGamesDev <yesterdiscord7@gmail.com>
2024-10-13 14:34:41 +0200

    m

--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'modulo',#AGREGANDO MODULO PERSONALIZADO (ver crear.txt)
-    'datos'
+    'datos',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'djangobasico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#datos por defecto para conectar con sqlite3 (db.sqlite3)
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+    #datos para conectarse a postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "djangopostgre",
+        "USER": "postgres",
+        "PASSWORD": "123456789",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
