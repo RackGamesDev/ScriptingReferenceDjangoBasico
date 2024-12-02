ece34d7155b20d0e9229d1f87743b2958a3104fd
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 02:20:09 +0100

    Update urls.py

--- datos/urls.py
+++ datos/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test, name="test"),#esta url seria accesibles en dominio.x/datos/test
+    path('test/', views.test, name="test"),#Esta url seria accesibles en dominio.x/datos/test
     path('create/', views.create, name="create"),
     path('delete/', views.delete, name="delete"),
     path('consultas/', views.consultas, name="consultas"),
