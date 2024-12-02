1ea6de43cb36ba3d5e6cec360a9c5bb3e922bcce
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 02:20:26 +0100

    Update models.py

--- datos/models.py
+++ datos/models.py
@@ -5,7 +5,7 @@ from datetime import date
 
 #CADA VEZ QUE SE ACTUALICE ESTE ARCHIVO: python manage.py makemigrations ; python manage.py migrate
 
-class Usuario(models.Model):#creando un modelo para un objeto de la base de datos
+class Usuario(models.Model):#Creando un modelo para un objeto de la base de datos
     nombre = models.CharField(max_length=12, null=False, default="a")#sus propiedades y como funcionan (automaticamente traducidas a sql)
     dinero = models.IntegerField(default=0)
     activo = models.BooleanField(default=False)
@@ -16,14 +16,14 @@ class Usuario(models.Model):#creando un modelo para un objeto de la base de dato
         return self.nombre
 
 class Publicacion(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)#clave foranea para relacionar entidades (la publicacion pertenece a un usuario) (se podrian hacer relaciones 1:1, 1:n y n:m segun como se hagan las claves)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)#Clave foranea para relacionar entidades (la publicacion pertenece a un usuario) (se podrian hacer relaciones 1:1, 1:n y n:m segun como se hagan las claves)
     nombre = models.CharField(max_length=10, null=False, default="a")
     puntuacion = models.FloatField(null=False, default=0.4)
     def __str__(self):
-        return self.usuario.nombre #accediendo a las propiedades de un objeto conectado
+        return self.usuario.nombre #Accediendo a las propiedades de un objeto conectado
 
 class Grupo(models.Model):
     nombre = models.CharField(max_length=10, null=False, default="a")
-    usuarios = models.ManyToManyField(Usuario) #para relaciones muchos-muchos, esto crearia una tabla extra en sql (se puede hacer manualmente con otro modelo)
+    usuarios = models.ManyToManyField(Usuario) #Para relaciones muchos-muchos, esto crearia una tabla extra en sql (se puede hacer manualmente con otro modelo)
     def __str__(self):
         return self.nombre
