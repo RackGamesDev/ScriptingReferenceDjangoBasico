28da75c609009a405abb4402c78deaf4c021d5e5
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 02:16:22 +0100

    Update views.py

--- peticiones/views.py
+++ peticiones/views.py
@@ -6,16 +6,16 @@ def pruebas(request):
     return render(request, 'peticiones/index.html', {})
 
 def recibePeticion(request):
-    print(request.method) #el verbo de la peticion http
-    print(request.COOKIES) #las cookies de la peticion
-    print(request.path) #destino de la peticion
-    print(request.FILES) #en caso de haber enviado archivos, aparecen aqui
-    print(request.META) #muchos datos del cliente
+    print(request.method) #El verbo de la peticion http
+    print(request.COOKIES) #Las cookies de la peticion
+    print(request.path) #Destino de la peticion
+    print(request.FILES) #En caso de haber enviado archivos, aparecen aqui
+    print(request.META) #Muchos datos del cliente
 
     data = request.body
     data = str(data, encoding='utf-8')
     data = json.loads(data)
-    print(data['texto']) #el body en formato dictionary de python
+    print(data['texto']) #El body en formato dictionary de python
     print(data['objeto']['nombre'])
 
-    return HttpResponse(json.dumps({"respuesta": "bien", "objeto": {"propiedad": 34}})) #para devolver un json
+    return HttpResponse(json.dumps({"respuesta": "bien", "objeto": {"propiedad": 34}})) #Para devolver un json
