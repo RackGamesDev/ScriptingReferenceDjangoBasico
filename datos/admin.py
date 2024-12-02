6118002dd97b1272b3f6811ed84168af4ad54a7c
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 02:20:40 +0100

    Update admin.py

--- datos/admin.py
+++ datos/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Publicacion
 
-class PublicacionAdmin(admin.ModelAdmin): #para especificar mas como se mostrara un modelo en el panel
+class PublicacionAdmin(admin.ModelAdmin): #Para especificar mas como se mostrara un modelo en el panel
     list_display = ("usuario", "nombre", "puntuacion")
     search_fields = ("nombre",)
     list_filter = ("nombre",)
 
 # Register your models here.
 
-admin.site.register(Publicacion, PublicacionAdmin) #cada modelo que aparecera en el panel de django (para poner ese filtro poner tambien el xAdmin)
+admin.site.register(Publicacion, PublicacionAdmin) #Cada modelo que aparecera en el panel de django (para poner ese filtro poner tambien el xAdmin)
